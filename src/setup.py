bf542d849bd48bccc490df46f50b62b5ea8511c2
peezhu <78460565+peezhu@users.noreply.github.com>
2021-07-30 11:37:08 -0400

    Task: Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='calculator',
+      version='0.0.1',
+      desription="calculator Project by Anthony Carl Sison",
+      author="Anthony Carl Sison",
+      author_email="ats43@njit.edu",
+      url="",
+      packages=find_packages(),
+      )
